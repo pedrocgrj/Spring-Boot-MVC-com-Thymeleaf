02fed0eb7293357b8643d3d65654f55546b710e5
Pedro Henrique <pedrohenrique.midas@gmail.com>
2020-04-03 19:18:17 -0300

    Atualização no Cargo
    Adicionando comportamentos de Editar, Salvar, Excluir e Listar Departamentos com mensagem personalizadas.
    Adicioanndo classe customizada DepartamentoConverter.

--- demo-spring-mvc/src/main/java/com/curso/boot/web/controller/CargoController.java
+++ demo-spring-mvc/src/main/java/com/curso/boot/web/controller/CargoController.java
@@ -1,20 +1,77 @@
 package com.curso.boot.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.curso.boot.domain.Cargo;
+import com.curso.boot.domain.Departamento;
+import com.curso.boot.service.CargoService;
+import com.curso.boot.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
+	
+	@Autowired
+	private CargoService cargoService;
+	
+	@Autowired
+	private DepartamentoService departamentoService;
 
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Cargo cargo) {
 		return "/cargo/cadastro";
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("cargos", cargoService.buscarTodos());
 		return "/cargo/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.salvar(cargo);
+		attr.addFlashAttribute("success", "Cargo inserido com sucesso.");
+		return "redirect:/cargos/cadastrar";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("cargo", cargoService.buscarPorId(id));
+		return "/cargo/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.editar(cargo);
+		attr.addFlashAttribute("success", "Cargo editado com sucesso.");
+		return "redirect:/cargos/cadastrar";
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
+		
+		if (cargoService.cargoTemFuncionarios(id)) {
+			attr.addAttribute("fail", "Cargo não removido. Possui Funcionário(s) vinculado(s).");
+		} else {
+			cargoService.excluir(id);
+			attr.addAttribute("success", "Cargo excluído com sucesso.");
+		}
+		return "redirect:/cargos/listar";
+	}
+	
+	@ModelAttribute("departamentos")
+	public List<Departamento> listaDeDepartamentos(){
+		return departamentoService.buscarTodos();
 	}
 }
